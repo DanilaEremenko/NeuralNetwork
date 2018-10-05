9f8c40d336146fa00a0c4f5b34519f9ca07bdc64
Danila <danilaeremenko@mail.ru>
2018-10-05 13:21:12 +0300

    Matplotlib круто

--- B_LogicalFunc.py
+++ B_LogicalFunc.py
@@ -1,13 +1,38 @@
+import matplotlib.pyplot as plt
+
+
 def func(x1: int, x2: int, x3: int, x4: int, x5: int):
     return x1 & x2 | x3 | x4 & x5
 
 
-n = 0
-print('number \t operands \t\t result')
-for x1 in range(0, 2):
-    for x2 in range(0, 2):
-        for x3 in range(0, 2):
-            for x4 in range(0, 2):
-                for x5 in range(0, 2):
-                    print(n, '\t\t', x1, x2, x3, x4, x5, '\t\t',func(x1, x2, x3, x4, x5))
-                    n += 1
+if __name__ == '__main__':
+
+    listX1 = []
+    listX2 = []
+    listY1 = []
+    listY2 = []
+
+    n = 1
+    print('number \t operands \t\t result')
+    for x1 in range(0, 2):
+        for x2 in range(0, 2):
+            for x3 in range(0, 2):
+                for x4 in range(0, 2):
+                    for x5 in range(0, 2):
+                        result = func(x1, x2, x3, x4, x5)
+                        if (result):
+                            # listX1.append(result)
+                            listX1.append(n)
+                            listY1.append(n)
+                        else:
+                            # listX2.append(result)
+                            listX2.append(n)
+                            listY2.append(n)
+
+                        print(n, '\t\t', x1, x2, x3, x4, x5, '\t\t', func(x1, x2, x3, x4, x5))
+                        n += 1
+
+    plt.plot(listX1, listY1, '.b')
+    plt.plot(listX2, listY2, '.r')
+
+    plt.show()
--- D_DivIntoNClasses.py
+++ D_DivIntoNClasses.py
@@ -1,10 +1,55 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import random as rand
 import math as m
 
 
-#def getFirstClass(size=100, radius=10, centerX=50, centerY=50):
+def isAcceptableOval(x, y, xCenter=30, yCenter=30, radVert=10, radHor=10):
+    if not m.pow((x - xCenter) / radHor, 2) + m.pow((y - yCenter) / radVert, 2) <= 1:
+        return False
+    return True
 
 
-def firstFunc(x, y, size):
-    return x * m.sin(x * y) * m.atan(y)
+def isAcceptableRect(x, y, xMin=0, yMin=0, xMax=0, yMax=0):
+    if not (range(xMin, xMax).__contains__(x) and range(yMin, yMax).__contains__(y)):
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    pointNumber = 1500
+    xCoor = []
+    yCoor = []
+
+    # FirstFigure
+    for i in range(0, pointNumber):
+        x = rand.randint(0, 200)
+        y = rand.randint(0, 200)
+        while (not isAcceptableRect(x, y, xMin=30, yMin=30, xMax=70, yMax=70)):
+            x = rand.randint(0, 200)
+            y = rand.randint(0, 200)
+        xCoor.append(x)
+        yCoor.append(y)
+
+    plt.plot(xCoor, yCoor, '.r')
+
+    xCoor.clear()
+    yCoor.clear()
+
+
+    # SecondFigure
+    for i in range(0, pointNumber):
+        x = rand.randint(0, 200)
+        y = rand.randint(0, 200)
+        while (not isAcceptableOval(x, y, xCenter=100, yCenter=120, radHor=30, radVert=30) or
+               isAcceptableOval(x, y, xCenter=130, yCenter=120, radHor=30, radVert=30)
+               or isAcceptableRect(x, y, xMin=60, yMin=110, xMax=80, yMax=130)):
+            x = rand.randint(0, 200)
+            y = rand.randint(0, 200)
+        xCoor.append(x)
+        yCoor.append(y)
+
+    plt.plot(xCoor, yCoor, '.b')
+
+    plt.xlim(0, 200)
+    plt.ylim(0, 200)
+    plt.show()
