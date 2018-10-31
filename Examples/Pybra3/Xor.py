d71ff9c79449f8388d62afca6fbd97ffeccec615
DanilaEremenko <danilaeremenko@mail.ru>
2018-10-31 22:55:38 +0300

    -Hear,work!

--- Examples/Pybra3/Xor.py
+++ Examples/Pybra3/Xor.py
@@ -1,6 +1,8 @@
+import random
+from math import fabs
 from pybrain3.datasets import SupervisedDataSet
-from pybrain3.tools.shortcuts import buildNetwork
 from pybrain3.supervised.trainers import BackpropTrainer
+from pybrain3.tools.shortcuts import buildNetwork
 
 ds = SupervisedDataSet(2, 1)
 ds.addSample((0, 0), 0)
@@ -8,14 +10,37 @@ ds.addSample((0, 1), 1)
 ds.addSample((1, 0), 1)
 ds.addSample((1, 1), 0)
 
-net = buildNetwork(2, 3, 1)
-trainer = BackpropTrainer(net, ds)
+ER_THRESHOLD = 1
 
-print(trainer.trainUntilConvergence())
+networkNumber = 0
+i = 0
 
-print(net.activate([0,0]))
-print(net.activate([1,1]))
-print(net.activate([0,1]))
-print(net.activate([1,0]))
+while i < 2:
 
+    networkNumber += 1
+    print(networkNumber)
 
+    hiddenLayerNumber = random.randint(0, 50)
+    print('hiddenLayerNumber = ', hiddenLayerNumber)
+
+    net = buildNetwork(2, hiddenLayerNumber, 1)
+
+    trainer = BackpropTrainer(net, ds)
+
+    trainer.trainUntilConvergence()
+
+    currentError = fabs(net.activate([0, 0]) - 0.0)
+    currentError += fabs(net.activate([0, 1]) - 1.0)
+    currentError += fabs(net.activate([1, 0]) - 1.0)
+    currentError += fabs(net.activate([1, 1]) - 0.0)
+    print('currentError = ', currentError)
+
+    if currentError < ER_THRESHOLD:
+        print('Results:')
+        print(net.activate([0, 0]))
+        print(net.activate([0, 1]))
+        print(net.activate([1, 0]))
+        print(net.activate([1, 1]))
+        i += 1
+
+    print('-------------------')
