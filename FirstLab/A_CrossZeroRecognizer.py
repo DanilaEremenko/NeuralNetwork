c2da5f075739fb02fd4290c22f918c444b8c4e1b
DanilaEremenko <danilaeremenko@mail.ru>
2018-11-16 11:32:04 +0300

    -separate graphic drawing added

--- FirstLab/A_CrossZeroRecognizer.py
+++ FirstLab/A_CrossZeroRecognizer.py
@@ -4,14 +4,13 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
 
-from TfWithKeras.GUI_REPORTER import plot_history
+import TfWithKeras.GUI_REPORTER as gr
 
 from keras.preprocessing import image
 import ZeroLab.Programms.A_CrossZero as dataset
 
 if __name__ == '__main__':
-    np.random.seed(42)
-
+    #np.random.seed(42)
     # x_train-image of numbers
     # y_train-answer of NN
     (x_train, y_train) = dataset.load_data(10, "A_CZ", images_size=(64, 64))
@@ -29,10 +28,9 @@ if __name__ == '__main__':
     # batch_size define speed of studying
     history = model.fit(x_train, y_train, batch_size=200, nb_epoch=20, verbose=1)
 
-    #score = model.evaluate(x_test, y_test, verbose=1)
-    #print "accuracy on testin data %.f%%" % (score[1] * 100)
+    # score = model.evaluate(x_test, y_test, verbose=1)
+    # print "accuracy on testin data %.f%%" % (score[1] * 100)
 
-
-    plot_history(history, 'HISTORY_20.png', save=True, show=False)
+    gr.plot_history_separte(history,save_path_acc="ACC.png",save_path_loss="LOSS.png",save=True,show=False)
 
     # model.save('MNIST_MODEL_200.h5')
--- TfWithKeras/GUI_REPORTER.py
+++ TfWithKeras/GUI_REPORTER.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
 def plot_history(history, save_path, save=False, show=True):
     plt.figure()
     plt.xlabel('Epoch')
@@ -17,3 +16,23 @@ def plot_history(history, save_path, save=False, show=True):
         plt.show()
     if save:
         plt.savefig(save_path, dpi=200)
+
+
+def plot_history_separte(history, save_path_acc, save_path_loss, save=False, show=True):
+
+    plot_graphic(x=history.epoch, y=np.array(history.history['loss']), label='Train Loss', save_path=save_path_loss,
+                 save=save, show=show)
+    plot_graphic(x=history.epoch, y=np.array(history.history['acc']), label='Train Acc', save_path=save_path_acc,
+                 save=save, show=show)
+
+
+def plot_graphic(x, y, label, save_path, save, show):
+    plt.plot(x, y, label=label)
+
+    plt.legend()
+
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(save_path, dpi=200)
+    plt.close()
