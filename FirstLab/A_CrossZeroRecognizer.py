f7c34de51ef79c929738827e554dc73134d6f97c
danila <danilaeremenko@mail.ru>
2018-11-16 23:46:47 +0300

    -train data was loaded

--- FirstLab/A_CrossZeroRecognizer.py
+++ FirstLab/A_CrossZeroRecognizer.py
@@ -24,23 +24,23 @@ if __name__ == '__main__':
 
     x_train.shape = (ex_num, image_size[0] * image_size[1])
     y_train.shape = (ex_num, 16)
-    #
-    # x_train = x_train / 255.0
-    #
-    # model = Sequential()
-    #
-    # # 800 neurons with 784 input,initialize - normal distribution
-    # model.add(Dense(800, input_dim=image_size[0] * image_size[1], init='normal', activation='relu'))
-    # model.add(Dense(16, init='normal', activation='softmax'))
-    #
-    # model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
-    #
-    # # batch_size define speed of studying
-    # history = model.fit(x_train, y_train, batch_size=200, nb_epoch=20, verbose=1)
-    #
-    # # score = model.evaluate(x_test, y_test, verbose=1)
-    # # print "accuracy on testin data %.f%%" % (score[1] * 100)
-    #
-    # gr.plot_history_separte(history, save_path_acc="ACC.png", save_path_loss="LOSS.png", save=True, show=False)
-    #
-    # # model.save('MNIST_MODEL_200.h5')
+
+    x_train = x_train / 255.0
+
+    model = Sequential()
+
+    # 800 neurons with 784 input,initialize - normal distribution
+    model.add(Dense(800, input_dim=image_size[0] * image_size[1], init='normal', activation='relu'))
+    model.add(Dense(16, init='normal', activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
+
+    # batch_size define speed of studying
+    history = model.fit(x_train, y_train, batch_size=200, nb_epoch=20, verbose=1)
+
+    # score = model.evaluate(x_test, y_test, verbose=1)
+    # print "accuracy on testin data %.f%%" % (score[1] * 100)
+
+    gr.plot_history_separte(history, save_path_acc="ACC.png", save_path_loss="LOSS.png", save=True, show=False)
+
+    # model.save('MNIST_MODEL_200.h5')
