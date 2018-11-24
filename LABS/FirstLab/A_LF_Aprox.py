badbd2a945de56496adaf3e5e1931e2dd4fa504b
oxovu <vlada318@live.com>
2018-11-24 23:33:56 +0300

    let's pretend that it works, OK?!

--- LABS/FirstLab/A_LF_Aprox.py
+++ LABS/FirstLab/A_LF_Aprox.py
@@ -9,24 +9,27 @@ import numpy as np
 
 if __name__ == '__main__':
     first_layer_nur = 10
-    lr = 0.45
+    second_layer_nur = 9
+    lr = 0.0675
     batch_size = 1
-    epochs = 5
+    epochs = 100
     verbose = 1
 
     y_train = np.array([[1, 1, 0, 0], [1, 1, 1, 0], [0, 1, 1, 1], [0, 0, 0, 1]])
 
-    (x_train, y_train) = dataset1.load_data(y_train)
+    (x_train, y_train) = dataset1.load_data(y_train, show=False)
 
     model = Sequential()
 
-    model.add(Dense(first_layer_nur, input_dim=x_train.shape[1], init='normal', activation='relu'))
-    model.add(Dense(1, init='normal', activation='hard_sigmoid'))
+    model.add(Dense(first_layer_nur, input_dim=x_train.shape[1], init='he_normal', activation='relu'))
+    model.add(Dense(second_layer_nur, init='glorot_normal', activation='linear'))
+    model.add(Dense(1, init='he_normal', activation='hard_sigmoid'))
 
     model.compile(loss='mean_squared_error', optimizer=SGD(lr=lr), metrics=['accuracy'])
 
     # batch_size define speed of studying
     history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=verbose)
 
+    print("\naccuracy on train data\t %.f%%" % (history.history['acc'][epochs - 1] * 100))
     gr.plot_history_separte(history, save_path_acc="ACC.png", save_path_loss="LOSS.png",
                             save=True, show=False)
