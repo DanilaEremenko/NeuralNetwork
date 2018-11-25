50a3ac15a884b396f37fafc15a5d4dc7f526fc69
oxovu <vlada318@live.com>
2018-11-25 23:33:41 +0300

    85% it becomes not so fun

--- LABS/FirstLab/D_N_Class_Divisor.py
+++ LABS/FirstLab/D_N_Class_Divisor.py
@@ -8,4 +8,32 @@ import LABS.ZeroLab.Programms.D_DivIntoNClasses as dataset4
 if __name__ == '__main__':
     train_size = 4000
 
-    (x_train, y_train), (x_test, y_test) = dataset4.load_data(train_size=train_size, show=True)
+    first_layer_nur = 40
+    second_layer_nur = 20
+    third_layer_nur = 5
+    lr = 0.75
+    batch_size = 15
+    epochs = 30
+    verbose = 1
+
+    (x_train, y_train), (x_test, y_test) = dataset4.load_data(train_size=train_size, show=False)
+
+    model = Sequential()
+
+    model.add(Dense(first_layer_nur, input_dim=x_train.shape[1], init='glorot_normal', activation='relu'))
+    model.add(Dense(second_layer_nur, init='glorot_normal', activation='linear'))
+    model.add(Dense(third_layer_nur, init='glorot_normal', activation='linear'))
+    model.add(Dense(1, init='he_normal', activation='hard_sigmoid'))
+
+    model.compile(loss='mean_squared_error', optimizer=SGD(lr=lr), metrics=['accuracy'])
+
+    # batch_size define speed of studying
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=verbose)
+
+    score = model.evaluate(x_test, y_test, verbose=1)
+
+    print("\naccuracy on train data\t %.f%%" % (history.history['acc'][epochs - 1] * 100))
+    print("\naccuracy on testing data %.f%%" % (score[1] * 100))
+    # print("loss on train data %.f%%" % (history.history['loss'][history.history] * 100))
+    gr.plot_history_separte(history, save_path_acc="ACC.png", save_path_loss="LOSS.png",
+                            save=True, show=True)
