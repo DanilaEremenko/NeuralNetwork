2f2fb3b8eeb08de906f6af849fb999362d4e405f
danila <danilaeremenko@mail.ru>
2018-11-29 20:25:07 +0300

    -explore layer.set_weights()

--- LABS/FirstLab/A_LF_Aproximator.py
+++ LABS/FirstLab/A_LF_Aproximator.py
@@ -6,31 +6,30 @@ from keras.optimizers import SGD
 import ADDITIONAL.GUI_REPORTER as gr
 import numpy as np
 
+from keras.utils.vis_utils import plot_model
+
 import LABS.ZeroLab.A_CrossZero as dataset1
 
 if __name__ == '__main__':
-    first_layer_nur = 10
-    second_layer_nur = 9
-    lr = 0.0675
-    batch_size = 1
-    epochs = 100
-    verbose = 1
-
-    y_train = np.array([[1, 1, 0, 0], [1, 1, 1, 0], [0, 1, 1, 1], [0, 0, 0, 1]])
+    y_train = np.array([[1, 1, 1, 1],
+                        [0, 1, 1, 0],
+                        [0, 1, 1, 0],
+                        [0, 0, 0, 0]])
 
     (x_train, y_train) = dataset1.load_data(y_train, show=False)
 
     model = Sequential()
 
-    model.add(Dense(first_layer_nur, input_dim=x_train.shape[1], init='he_normal', activation='relu'))
-    model.add(Dense(second_layer_nur, init='glorot_normal', activation='linear'))
-    model.add(Dense(1, init='he_normal', activation='hard_sigmoid'))
+    model.add(Dense(4, input_dim=x_train.shape[1], activation='hard_sigmoid',
+                    weights=list([np.array([[0.0, 0.0, 1.0, 1.0],
+                                            [1.0, -1.0, 0.0, 0.0]], dtype=float),
+                                  np.array([-0.25, 0.75, -0.25, -0.75], dtype=float)])))
+    model.add(Dense(2, activation='hard_sigmoid',
+                    weights=list([np.array([[1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [0.0, 1.0]], dtype=float),
+                                  np.array([-2.5, -0.5], dtype=float)])))
+    model.add(Dense(1, activation='hard_sigmoid',
+                    weights=list([np.array([[1.0], [1.0]], dtype=float),
+                                  np.array([-0.5], dtype=float)])))
 
-    model.compile(loss='mean_squared_error', optimizer=SGD(lr=lr), metrics=['accuracy'])
 
-    # batch_size define speed of studying
-    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=verbose)
-
-    print("\naccuracy on train data\t %.f%%" % (history.history['acc'][epochs - 1] * 100))
-    gr.plot_history_separte(history, save_path_acc="ACC.png", save_path_loss="LOSS.png",
-                            save=True, show=False)
+    plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
