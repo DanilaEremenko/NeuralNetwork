5b857f3a69e60154c5c1d2be353884a6e6d8d17f
danila <danilaeremenko@mail.ru>
2018-12-07 14:26:42 +0300

    -2/A fixed

--- LABS/SecondLab/A_ToolExploring.py
+++ LABS/SecondLab/A_ToolExploring.py
@@ -11,45 +11,46 @@ def plot_weights():
 
 if __name__ == '__main__':
     ne_num = 4
+    in_num = 1
     seed = 42
 
     print("\n--------------Zeros--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1, kernel_initializer=wi.Zeros(), bias_initializer=wi.Zeros()))
+    model.add(Dense(ne_num, input_dim=in_num, kernel_initializer=wi.Zeros(), bias_initializer=wi.Zeros()))
     print(model.get_weights())
 
     print("\n--------------Ones--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1, kernel_initializer=wi.Ones(), bias_initializer=wi.Ones()))
+    model.add(Dense(ne_num, input_dim=in_num, kernel_initializer=wi.Ones(), bias_initializer=wi.Ones()))
     print(model.get_weights())
 
     print("\n--------------Constant--------------\n")
     model = Sequential()
     model.add(
-        Dense(ne_num, input_dim=1, kernel_initializer=wi.Constant(value=2.0), bias_initializer=wi.Constant(value=2.0)))
+        Dense(ne_num, input_dim=in_num, kernel_initializer=wi.Constant(value=2.0), bias_initializer=wi.Constant(value=2.0)))
     print(model.get_weights())
 
     print("\n--------------RandomNormal--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1, kernel_initializer=wi.RandomNormal(mean=0.0, stddev=0.05, seed=seed),
+    model.add(Dense(ne_num, input_dim=in_num, kernel_initializer=wi.RandomNormal(mean=0.0, stddev=0.05, seed=seed),
                     bias_initializer=wi.RandomNormal(mean=0.0, stddev=0.05, seed=seed)))
     print(model.get_weights())
 
     print("\n--------------RandomUniform--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1, kernel_initializer=wi.RandomUniform(minval=-0.05, maxval=0.05, seed=seed),
+    model.add(Dense(ne_num, input_dim=in_num, kernel_initializer=wi.RandomUniform(minval=-0.05, maxval=0.05, seed=seed),
                     bias_initializer=wi.RandomUniform(minval=-0.05, maxval=0.05, seed=seed)))
     print(model.get_weights())
 
     print("\n--------------TruncatedNormal--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1, kernel_initializer=wi.TruncatedNormal(mean=0.0, stddev=0.05, seed=seed),
+    model.add(Dense(ne_num, input_dim=in_num, kernel_initializer=wi.TruncatedNormal(mean=0.0, stddev=0.05, seed=seed),
                     bias_initializer=wi.TruncatedNormal(mean=0.0, stddev=0.05, seed=seed)))
     print(model.get_weights())
 
     print("\n--------------VarianceScaling--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1,
+    model.add(Dense(ne_num, input_dim=in_num,
                     kernel_initializer=wi.VarianceScaling(scale=1.0, mode='fan_in', distribution='normal', seed=seed),
                     bias_initializer=wi.VarianceScaling(scale=1.0, mode='fan_in', distribution='normal', seed=seed)))
     print(model.get_weights())
@@ -71,42 +72,42 @@ if __name__ == '__main__':
 
     print("\n--------------lecun_normal--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1,
+    model.add(Dense(ne_num, input_dim=in_num,
                     kernel_initializer=wi.lecun_normal(seed=seed),
                     bias_initializer=wi.lecun_normal(seed=seed)))
     print(model.get_weights())
 
     print("\n--------------lecun_uniform--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1,
+    model.add(Dense(ne_num, input_dim=in_num,
                     kernel_initializer=wi.lecun_uniform(seed=seed),
                     bias_initializer=wi.lecun_uniform(seed=seed)))
     print(model.get_weights())
 
     print("\n--------------glorot_normal--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1,
+    model.add(Dense(ne_num, input_dim=in_num,
                     kernel_initializer=wi.glorot_normal(seed=seed),
                     bias_initializer=wi.glorot_normal(seed=seed)))
     print(model.get_weights())
 
     print("\n--------------glorot_uniform--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1,
+    model.add(Dense(ne_num, input_dim=in_num,
                     kernel_initializer=wi.glorot_uniform(seed=seed),
                     bias_initializer=wi.glorot_uniform(seed=seed)))
     print(model.get_weights())
 
     print("\n--------------he_normal--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1,
+    model.add(Dense(ne_num, input_dim=in_num,
                     kernel_initializer=wi.he_normal(seed=seed),
                     bias_initializer=wi.he_normal(seed=seed)))
     print(model.get_weights())
 
     print("\n--------------he_uniform--------------\n")
     model = Sequential()
-    model.add(Dense(ne_num, input_dim=1,
+    model.add(Dense(ne_num, input_dim=in_num,
                     kernel_initializer=wi.he_uniform(seed=seed),
                     bias_initializer=wi.he_uniform(seed=seed)))
     print(model.get_weights())
