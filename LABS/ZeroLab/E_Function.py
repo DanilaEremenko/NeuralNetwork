79a0391c38b09f24a5c00923288e99e54ecfe26a
danila <danilaeremenko@mail.ru>
2018-12-08 16:28:42 +0300

    -dataset 5 fixed

--- LABS/ZeroLab/E_Function.py
+++ LABS/ZeroLab/E_Function.py
@@ -1,4 +1,4 @@
-#dataset5
+# dataset5
 
 import matplotlib.pyplot as plt
 import math as m
@@ -10,37 +10,36 @@ def function(x):
     return 0.25 + x / 200.0 + 0.25 * m.sin(2.0 / 3.0 * x * m.sin(x / 50.0 + 3.0 * m.pi / 2.0))
 
 
-def load_data(train_size=200,show=False):
-    
-    test_size = int(train_size*0.2)
-    
-    h = 1.0 / float(train_size+test_size)
-    
-    
+def load_data(train_size=200, show=False):
+    test_size = int(train_size * 0.2)
+
+    h = 1.0 / float(train_size + test_size)
+
     x_train = np.empty(0)
     y_train = np.empty(0)
-    
+
     x_test = np.empty(0)
     y_test = np.empty(0)
-    
+
     i = 0
-    for x in np.arange(0.0,1.0,h,dtype = float):
+    for x in np.arange(0.0, 1.0, h, dtype=float):
         if i % 5 == 0:
             x_test = np.append(x_test, x)
-            y_test = np.append(y_test,function(x))
+            y_test = np.append(y_test, function(x))
         else:
-            x_train = np.append(x_train,x)
-            y_train = np.append(y_train,function(x))
+            x_train = np.append(x_train, x)
+            y_train = np.append(y_train, function(x))
 
         i += 1
-        
-    
-    plt.plot(x_train,y_train,'.')
-    plt.plot(x_test,y_test,'.')
-    plt.legend(('train_data','test_data'),loc='upper left', shadow=True)
+
+    plt.plot(x_train, y_train, '.')
+    plt.plot(x_test, y_test, '.')
+    plt.legend(('train_data', 'test_data'), loc='upper left', shadow=True)
     if show:
         plt.show()
     plt.close()
 
-    return (x_train,y_train),(x_test,y_test)
+    x_train = np.append(x_train, np.ones(x_train.size)).reshape(2, x_train.size)
+    x_test = np.append(x_test, np.ones(x_test.size)).reshape(2, x_test.size)
 
+    return (x_train, y_train), (x_test, y_test)
