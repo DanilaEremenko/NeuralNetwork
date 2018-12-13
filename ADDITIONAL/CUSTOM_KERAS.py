b6bf4f717f0b9896930ce228cb5f61f73f77f507
danila <danilaeremenko@mail.ru>
2018-12-13 22:12:21 +0300

    Psh,psh, Neural Network, FREEZE! You're surrounded. We have found early stopper by loss!

--- ADDITIONAL/CUSTOM_KERAS.py
+++ ADDITIONAL/CUSTOM_KERAS.py
@@ -1,6 +1,24 @@
 import keras.backend as K
+from keras.callbacks import Callback
 
 
 def hard_lim(x):
     return K.cast(K.greater_equal(x, 0), K.floatx())
 
+
+class EarlyStoppingByLossVal(Callback):
+    def __init__(self, monitor='val_loss', value=0.00001, verbose=0):
+        super(Callback, self).__init__()
+        self.monitor = monitor
+        self.value = value
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch, logs={}):
+        current = logs.get(self.monitor)
+        if current is None:
+            print("Early stopping requires %s available!" % self.monitor, RuntimeWarning)
+
+        if current < self.value:
+            if self.verbose > 0:
+                print("Epoch %05d: early stopping THR" % epoch)
+            self.model.stop_training = True
--- LABS/SecondLab/C_DifficultAprox.py
+++ LABS/SecondLab/C_DifficultAprox.py
@@ -1,26 +1,29 @@
+from __future__ import print_function
 from keras.models import Sequential
-from keras import callbacks
 from keras.layers import Dense
 from keras.optimizers import SGD, Adam, Adadelta
 import matplotlib.pyplot as plt
-from keras.utils.vis_utils import plot_model
 import numpy as np
-import keras.initializers as wi
 
 from LABS.ZeroLab import E_Function as dataset5
 import ADDITIONAL.GUI_REPORTER as gr
+from ADDITIONAL.CUSTOM_KERAS import EarlyStoppingByLossVal
+
+import os
 
 if __name__ == '__main__':
     np.random.seed(42)
     # 1,2 initializing
-
     train_size = 2000
     batch_size = 20
-    epochs = 1000
-    lr = 0.23
+    epochs = 100
+    lr = 0.01
     verbose = 1
+    opt_type = 0  # 0-SGD, 1 - SGD + Nesterov, 2 - Adam, 3 - Adadelta
+    opt_name = "None"
 
-    (x_train, y_train), (x_test, y_test) = dataset5.load_data(train_size=train_size, show=False, func_type='sin')
+    (x_train, y_train), (x_test, y_test) = dataset5.load_data(train_size=train_size, show=False)
+
     model = Sequential()
 
     model.add(
@@ -34,23 +37,48 @@ if __name__ == '__main__':
     # plot_model(model, to_file="C_Model.png", show_shapes=True, show_layer_names=True)
 
     # 3 setting stopper
-    # stopper = callbacks.EarlyStopping(monitor='acc', min_delta=0, patience=5, mode='max')
+    # callbacks.EarlyStopping(monitor='acc', min_delta=0, patience=5, mode='max')
+    callbacks = [EarlyStoppingByLossVal(monitor='val_loss', value=0.1, verbose=1)]
+
+    if opt_type == 0:
+        optimizer = SGD(lr)
+        opt_name = "SGD"
+    elif opt_type == 1:
+        optimizer = SGD(lr, nesterov=True)
+        opt_name = "SGD+Nesterov"
+    elif opt_type == 2:
+        optimizer = Adam(lr, nesterov=True)
+        opt_name = "Adam"
+    elif opt_type == 2:
+        optimizer = Adadelta()
+        opt_name = "Adadelta"
+    else:
+        Exception("Unexpected opt_type value")
 
     # 4 model fitting
-    model.compile(optimizer=SGD(lr=lr), loss='mae', metrics=['mae'])
+    model.compile(optimizer=Adam(lr=lr), loss='mae', metrics=['mae'])
 
     history = model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs,
-                        verbose=verbose)
+                        verbose=verbose, callbacks=callbacks, validation_data=(x_test, y_test))
+
+    print("last loss = %.f3" % history.history["loss"][history.epoch.__len__() - 1])
 
     score = model.evaluate(x_test, y_test, verbose=verbose)
 
-    # gr.plot_history_separte(history=history, save_path_acc="ACC.png", save_path_loss="LOSS.png",
-    #                         save=False, show=True, acc='mean_absolute_error')
+    dir_name = "C_"+opt_name + "_" + str(history.epoch.__len__()) + "_" + str(lr)
+
+    os.mkdir(dir_name)
+
+    gr.plot_graphic(x=history.epoch, y=np.array(history.history["loss"]), x_label='epochs', y_label='loss',
+                    title="loss" + ' history', save_path=dir_name + "/" + "loss.png", save=True, show=True)
 
     plt.plot(np.transpose(x_test)[0], y_test, '.-')
     plt.plot(np.transpose(x_test)[0], model.predict(x_test), '.-')
-    plt.legend(('function','approximation'), loc='lower left', shadow=True)
+    plt.legend(('function', 'approximation'), loc='lower left', shadow=True)
     plt.title('aproximation comparison\nlr = %.3f\nloss = %.4f' % (lr, score[0]))
 
+    plt.savefig(dir_name + "/" + "compare.png", dpi=200)
     plt.show()
     plt.close()
+
+    model.save(dir_name + "/" + dir_name + '.h5')
