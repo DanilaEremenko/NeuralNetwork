ea07a92f63147ba7406dc56fa0a1892d1638a4f9
danila <danilaeremenko@mail.ru>
2018-12-28 01:21:05 +0300

    -report added))))))))

--- LABS/FoutrhLab/BA_SOFM_Aprox.py
+++ LABS/FoutrhLab/BA_SOFM_Aprox.py
@@ -1,6 +1,7 @@
 from neupy import algorithms
 from matplotlib import pyplot as plt
 from neupy.exceptions import StopTraining
+import numpy as np
 
 from LABS.ZeroLab import E_Function as dataset5
 
@@ -8,6 +9,12 @@ from LABS.ZeroLab import E_Function as dataset5
 def on_epoch_end(model):
     if model.train_errors.last() < goal_loss:
         raise StopTraining("Training has been interrupted")
+
+
+def get_index(poss_code):
+    for i in range(poss_code.__len__()):
+        if poss_code[i] == 1:
+            return i
 
 
 if __name__ == '__main__':
@@ -22,7 +29,7 @@ if __name__ == '__main__':
 
     data = zip(x_train, y_train)
 
-    for m in [5, 10, 15]:
+    for m in [5]:
         sofm = algorithms.SOFM(
             n_inputs=2,
             n_outputs=m * m,
@@ -39,12 +46,33 @@ if __name__ == '__main__':
 
         )
 
-        sofm.train(data, epochs=100)
+        sofm.train(data, epochs=10)
 
         plt.plot(x_train, y_train, 'b.', label='real')
         plt.plot(sofm.weight[0], sofm.weight[1], 'r.', label='sofm weights')
 
         plt.legend(loc='upper right')
-        plt.title("SOFM\nm = %.d\n lr = %4.f" % (m, step))
+        plt.title("SOFM\nm = %.d\n lr = %.4f" % (m, step))
 
         plt.show()
+        plt.close()
+
+        weights_plt_x = []
+        weights_plt_y = []
+
+        for i in range(0, m * m):
+            weights_plt_x.append(np.empty(0))
+            weights_plt_y.append(np.empty(0))
+
+        for x in np.arange(0.0, 1.0, step=0.01):
+            for y in np.arange(0.0, 1.0, step=0.01):
+                i = get_index(sofm.predict(np.array([x, y]))[0])
+
+                weights_plt_x[i] = np.append(weights_plt_x[i], x)
+                weights_plt_y[i] = np.append(weights_plt_y[i], y)
+
+        for i in range(0, m * m):
+            plt.plot(weights_plt_x[i], weights_plt_y[i])
+        plt.title("Weights distribution for \nSOFM\nm = %.d\n lr = %.4f" % (m, step))
+        plt.show()
+        plt.close()
